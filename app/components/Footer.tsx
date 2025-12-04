d82aa7da1e5c6f663734312dab0d21ae190a72e1
redoakdigital <redoakdigital@protonmail.com>
2025-12-04 16:05:41 -0600

    Delete app directory

--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-12 border-t border-primary-900/30 text-center text-gray-500">
-      © 2025 Red Oak Digital • Since 2023
-    </footer>
-  )
-}
