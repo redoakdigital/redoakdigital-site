39f869f7a0e3bc828ac4e6bab93e23159de5851c
redoakdigital <redoakdigital@protonmail.com>
2025-12-04 10:53:56 -0600

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-12 border-t border-primary-900/30 text-center text-gray-500">
+      © 2025 Red Oak Digital • Since 2023
+    </footer>
+  )
+}
